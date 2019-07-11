1ff2fad9f39c8bb2fc58b2d14c056cffa2a66b05
Arman Yerkesh <a.yerkesh@gmail.com>
2019-07-11 15:39:00 +0300

    Make test ajax call

--- src/app/data.component.ts
+++ src/app/data.component.ts
@@ -1,4 +1,16 @@
+import $ from "jquery/dist/jquery.js";
 import { Component } from '@angular/core';
+
+const omdbUrl: string = 'http://www.omdbapi.com/?apikey=f17da8f8&';
+let omdbResponse: object;
+
+$.ajax({
+  url: omdbUrl + 't=Taxi',
+  success: function(result) {
+    omdbResponse = result;
+    console.log(omdbResponse);
+  }
+})
 
 @Component({
   selector: 'app-root',
