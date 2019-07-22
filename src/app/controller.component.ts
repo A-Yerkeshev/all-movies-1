d99aac9bc81b986422971384dc373d7f75f3eb65
Arman Yerkesh <a.yerkesh@gmail.com>
2019-07-22 10:21:03 +0300

    Show spinner while movies are loading

--- src/app/controller.component.ts
+++ src/app/controller.component.ts
@@ -3,6 +3,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { DataCollector } from './data.collector';
+import $ from "jquery/dist/jquery.js";
 
 const dataCollector = new DataCollector();
 
@@ -26,14 +27,26 @@ class ControllerComponent{
   changePage(newPageIndex: number) {
     // Check if new page index is valid
     if (newPageIndex > 0 && newPageIndex <= this.lastPage) {
-      // Check if new movies need to be loaded
-      if (newPageIndex > this.loadedMovies.length/this.itemsPerPage) {
-        this.loadedMovies = dataCollector.loadDefaultMovies(this.loadedMovies, this.itemsPerPage);
-      }
-      // Display movies on page
-      let firstMovieIndex: number = this.itemsPerPage*(newPageIndex-1);
-      this.displayedMovies = this.loadedMovies.slice(firstMovieIndex, firstMovieIndex+this.itemsPerPage);
-      this.currentPage = newPageIndex;
+      // Display loading spinner while new movies are loading
+      const spinner: string = `<div class="position-absolute w-100 bg-white text-center spinner-sheet">
+        <div class="spinner-border text-secondary mt-5" role="status"><span class="sr-only">Loading...</span></div>
+        </div>`;
+      $('.top-movies').append(spinner);
+      // To load movies asynchronously, wrap it into setTimeout function
+      let component = this;
+      setTimeout(function(component: ControllerComponent) {
+        console.log(component)
+        if (newPageIndex > component.loadedMovies.length/component.itemsPerPage) {
+          component.loadedMovies = dataCollector.loadDefaultMovies(component.loadedMovies, component.itemsPerPage);
+        }
+        // Display movies on page
+        let firstMovieIndex: number = component.itemsPerPage*(newPageIndex-1);
+        component.displayedMovies = component.loadedMovies.slice(
+          firstMovieIndex, firstMovieIndex+component.itemsPerPage);
+        component.currentPage = newPageIndex;
+        // Remove loading spinner
+        $('.spinner-sheet').remove();
+      }, 10, component)
     }
   }
 }
