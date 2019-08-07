1c43da1bd1a1b538bcab879a570f6ef0c87a3400
Arman Yerkesh <a.yerkesh@gmail.com>
2019-08-07 10:10:19 +0300

    Initialize DataService

---
+++ src/app/data.service.ts
@@ -0,0 +1,13 @@
+/* Service is responsible for collecting the data from JSON files, the server
+  responses through usage of API Communicator and from user inputs through components. Data then transmitted
+  to Home Page Component and Movie Component. */
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+}
--- src/app/movie.component.ts
+++ src/app/movie.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { DataCollector } from './data.collector';
+
+const dataCollector = new DataCollector();
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.html'
 })
 class MovieComponent{
-
+  currentMovie: object;
+  constructor() {
+    this.currentMovie = dataCollector.currentMovie
+  }
 }
 
 export { MovieComponent }
