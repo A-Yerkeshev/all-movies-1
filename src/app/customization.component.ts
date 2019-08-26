13270a55a34db44b7fe59c04736b76e82eadaf15
Arman Yerkesh <a.yerkesh@gmail.com>
2019-08-26 10:18:55 +0300

    Retrieve searched title from query params in SearchResult component

--- src/app/customization.component.ts
+++ src/app/customization.component.ts
@@ -1,7 +1,7 @@
 /* Component is responsible to organize the data recieved from Data Service according to user preferences. Component
 keeps track of user's preferences through usage of local storage of the browser and cookies. */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
--- src/app/search.result.component.ts
+++ src/app/search.result.component.ts
@@ -1,11 +1,19 @@
+/* Component is responsible for handling search queries made by user and rendering the results with search.result.html
+template. */
+
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
   templateUrl: './search.result.html'
 })
 export class SearchResultComponent{
+  searchTitle: string;
 
-  constructor() { }
+  constructor(private dataService: DataService, private route: ActivatedRoute) {
+    this.searchTitle = this.route.snapshot.queryParamMap.get('title');
+  }
 
 }
